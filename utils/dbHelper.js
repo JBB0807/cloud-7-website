ab43c8999e9b02c79e41117cd5efbadd2450311e
JBB0807 <104856796+JBB0807@users.noreply.github.com>
2025-03-18 11:45:08 -0700

    oved aws functions to Utils classes

---
+++ utils/dbHelper.js
@@ -0,0 +1,66 @@
+//
+// AWS Functions
+//
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-west-2" });
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+class dbHelper{};
+// AWS function to get the player score from cloud7_leaderboard
+//
+function getRankings() {
+    dynamoDB.scan({ TableName: "cloud7_leaderboard" }, (err, data) => {
+        if (err) console.error("Error scanning table:", err);
+        else {
+            console.log("All items:", data.Items);
+            return data.Items;      
+        }
+    });
+}
+
+// AWS function to get the player information from cloud7_player
+//
+function getPlayerInfo(id) {
+    const params = {
+        TableName: "cloud7_player",
+        KeyConditionExpression: "playerId = :id",
+        ExpressionAttributeValues: {
+            ":id": id
+        }
+    };
+
+    dynamoDB.query(params, (err, data) => {
+        if (err) console.error("Error querying table:", err);
+        else {
+            console.log("Player info:", data.Items);
+            return data.Items;
+        }
+    });
+}
+
+// AWS function to get the player score from cloud7_score
+//
+function getPlayerScore(id) {
+    const params = {
+        TableName: "cloud7_score",
+        KeyConditionExpression: "playerId = :id",
+        ExpressionAttributeValues: {
+            ":id": id
+        }
+    };
+
+    dynamoDB.query(params, (err, data) => {
+        if (err) console.error("Error querying table:", err);
+        else {
+            console.log("Player score:", data.Items);
+            return data.Items;
+        }
+    });
+}
+
+module.exports = {
+    dbHelper,
+    getRankings,
+    getPlayerInfo,
+    getPlayerScore
+};
